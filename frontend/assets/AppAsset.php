e73e7bb74360fb347b30c9774ed63ad310def82f
Khurshidbek <xurshidjon1607@gmail.com>
2023-02-27 17:39:48 +0500

    add any js files

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -27,8 +27,8 @@ class AppAsset extends AssetBundle
         'css/style.css'
     ];
     public $js = [
-        'js/vendor/modernizr-3.5.0.min.js',
-        'js/vendor/jquery-1.12.4.min.js',
+        'js/modernizr-3.5.0.min.js',
+        'js/jquery-1.12.4.min.js',
         'js/popper.min.js',
         'js/bootstrap.min.js',
         'js/owl.carousel.min.js',
