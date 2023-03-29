3d86d140fed0ebc5c69d1b7ab58dc40eef9f96c7
Khurshidbek <xurshidjon1607@gmail.com>
2023-03-29 10:57:19 +0500

    tours admin index

--- frontend/views/site/pages/posts.php
+++ frontend/views/site/pages/posts.php
@@ -133,45 +133,45 @@ use yii\bootstrap5\Html;
                         </ul>
                     </aside>
 
-                    <aside class="single_sidebar_widget popular_post_widget">
-                        <h3 class="widget_title">Recent Post</h3>
-                        <div class="media post_item">
-                            <?= Html::img('/template/img/post/post_1.png') ?>
-                            <div class="media-body">
-                                <a href="<?= Url::toRoute(['single-blog']) ?>">
-                                    <h3>From life was you fish...</h3>
-                                </a>
-                                <p>January 12, 2019</p>
-                            </div>
-                        </div>
-                        <div class="media post_item">
-                            <?= Html::img('/template/img/post/post_2.png') ?>
-                            <div class="media-body">
-                                <a href="<?= Url::toRoute(['single-blog']) ?>">
-                                    <h3>The Amazing Hubble</h3>
-                                </a>
-                                <p>02 Hours ago</p>
-                            </div>
-                        </div>
-                        <div class="media post_item">
-                            <?= Html::img('/template/img/post/post_3.png') ?>
-                            <div class="media-body">
-                                <a href="<?= Url::toRoute(['single-blog']) ?>">
-                                    <h3>Astronomy Or Astrology</h3>
-                                </a>
-                                <p>03 Hours ago</p>
-                            </div>
-                        </div>
-                        <div class="media post_item">
-                            <?= Html::img('/template/img/post/post_4.png') ?>
-                            <div class="media-body">
-                                <a href="<?= Url::toRoute(['single-blog']) ?>">
-                                    <h3>Asteroids telescope</h3>
-                                </a>
-                                <p>01 Hours ago</p>
-                            </div>
-                        </div>
-                    </aside>
+<!--                    <aside class="single_sidebar_widget popular_post_widget">-->
+<!--                        <h3 class="widget_title">Recent Post</h3>-->
+<!--                        <div class="media post_item">-->
+<!--                            --><?php//= Html::img('/template/img/post/post_1.png') ?>
+<!--                            <div class="media-body">-->
+<!--                                <a href="--><?//= Url::toRoute(['single-blog']) ?><!--">-->
+<!--                                    <h3>From life was you fish...</h3>-->
+<!--                                </a>-->
+<!--                                <p>January 12, 2019</p>-->
+<!--                            </div>-->
+<!--                        </div>-->
+<!--                        <div class="media post_item">-->
+<!--                            --><?php// Html::img('/template/img/post/post_2.png') ?>
+<!--                            <div class="media-body">-->
+<!--                                <a href="--><?//= Url::toRoute(['single-blog']) ?><!--">-->
+<!--                                    <h3>The Amazing Hubble</h3>-->
+<!--                                </a>-->
+<!--                                <p>02 Hours ago</p>-->
+<!--                            </div>-->
+<!--                        </div>-->
+<!--                        <div class="media post_item">-->
+<!--                            --><?php// Html::img('/template/img/post/post_3.png') ?>
+<!--                            <div class="media-body">-->
+<!--                                <a href="--><?php //Url::toRoute(['single-blog']) ?><!--">-->
+<!--                                    <h3>Astronomy Or Astrology</h3>-->
+<!--                                </a>-->
+<!--                                <p>03 Hours ago</p>-->
+<!--                            </div>-->
+<!--                        </div>-->
+<!--                        <div class="media post_item">-->
+<!--                            --><?//= Html::img('/template/img/post/post_4.png') ?>
+<!--                            <div class="media-body">-->
+<!--                                <a href="--><?//= Url::toRoute(['single-blog']) ?><!--">-->
+<!--                                    <h3>Asteroids telescope</h3>-->
+<!--                                </a>-->
+<!--                                <p>01 Hours ago</p>-->
+<!--                            </div>-->
+<!--                        </div>-->
+<!--                    </aside>-->
                 </div>
             </div>
         </div>
